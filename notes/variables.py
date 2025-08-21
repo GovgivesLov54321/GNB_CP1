edafe87bbc3217441855d47b77af8c87b41b2767
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-08-21 09:52:24 -0600

    Variables Notes (yeehaw)

---
+++ notes/variables.py
@@ -0,0 +1,8 @@
+# GNB - 1st - Variables Notes
+
+even_number = 2
+grade = 100
+animal = "Tortoise"
+print(grade)
+grade = 200 #reset variable
+print(grade)
---
+++ starters/seasons.py
@@ -0,0 +1,6 @@
+# GNB - 1st - Seasons
+last_name = input("What's your last name?: ")
+
+favorite_season = input("Ok, and what's your favorite season of the year?: ")
+
+print("WOW", last_name, "san, I like", favorite_season, "too!")
