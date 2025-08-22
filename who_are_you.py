e3e00284093f99b98c8796d3fdd6fb2073ecfead
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-08-22 09:39:58 -0600

    who are you (lite frfr *yawn*)

---
+++ who_are_you.py
@@ -0,0 +1,8 @@
+# GNB - 1st - 💡 Who Are You?
+nombre = input("Good day, User! What's your name? (only your name please): ")
+
+age = input("That's an... interesting name. How old are you right now? (only the number please): ")
+
+color = input("Dang alright, unc. Well, what's your favorite color?: ")
+
+print("That's a pretty color,", nombre + ", and I've met a lot of very cool people who are also", age, "years old and like the color", color + ", so MAYBE you'll also end up being a cool person!")
