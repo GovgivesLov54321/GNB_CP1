0596f5270faf281f67ade15b957dd994ad588965
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-09-02 09:52:51 -0600

    strings notes (lwk wasn't locked in)

---
+++ notes/strings.py
@@ -0,0 +1,45 @@
+# GNB - 1st - ☑️ Strings Notes
+
+#A string is a collection of symbols held together by quotation marks
+
+sentence = "The quick brown fox jumps over the lazy dog!"
+
+first_name = "Tio"
+mes = "Septeimbre"
+libro = "The Big, Bad Wolf"
+comida = "papas fritas"
+
+print(mes)
+
+#Called strings because
+
+#Make one by
+
+#We use strings because
+
+#It doesn't matter if I use single or double quotes
+
+#An escape character is
+
+#print(""Sigma"is so alpha") #No bueno
+print("'Sigma' is so alpha") #Bueno
+print('"Sigma" is so alpha') #Bueno
+
+print("\"Sigma\" is so alpha") #escape character
+
+#We get the length of our string by 
+
+print(len(sentence)) #length of string (amount of characters; includes spaces)
+
+#We concatanate strings by doing:
+
+last_name = "Alpha"
+full_name = first_name + last_name
+
+print(full_name)
+
+#I slice a string by
+print(mes[0:5])
+#And index is
+
+#We count indexes by 0 is 1 and 1 is 2, so on, so forth
