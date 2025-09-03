22deb3d803288503cfee90d5cf5f96c21cae8b1f
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-09-03 09:53:14 -0600

    madlib nearly done frfr

---
+++ projects/madlib.py
@@ -0,0 +1,19 @@
+# GNB - 1st - 🔨 Madlib
+# madlib frfr: My name is (name) and i am (number) years old. If I were president, I'd do a whole bunch of (adjective) things: 1. I would drive the biggest (color) (vehichle) in the country. And that car would go faster than any other (noun) in the world. \n2.Everyone would eat sweet and spicy (food) for breakfast, lunch, and dinner. \n3. I would live in the (famous building) and build a (gerundio) pool at her feet. \n4. School would be open for (number) days a year. \n5. I would give my friends the best jobs. I would nominate (a friend) to be the secretary of (the) (noun), and (another friend) would be vice (occupation)!!!
+
+age = input("Give me a number: ")
+name = input("Give me a name: ")
+homie_1 = input("Give me a friend's full name: ")
+adejective_1 = input("Give me an adjective (describes a noun): ")
+vehicle_color = input("Give me any color: ")
+vehicle = input("Give me a cool vehicle: ")
+noun_1 = vehicle
+comida = input("Give me the nastiest food you can think of: ")
+job_2 = input("Give me an... interesting job: ")
+home = input("Give me a famous building: ")
+gerund = input("Give me a gerund (verb that ends in -ing): ")
+job_1 = input("Give me a noun: ")
+homie_2 = input("Give me a different friend's full name: ")
+school_days = input("Give me a random number: ")
+
+print("My name is "+ name + " and I am " + age + " years old. If I were president, I'd do a whole bunch of "+ adejective_1 + " things:\n 1. I would drive the biggest " +vehicle_color + vehicle + " in the country. And that car would go faster than any other " + noun_1 + "  in the world. \n2.Everyone would eat sweet and spicy " + comida +" for breakfast, lunch, and dinner. \n3. I would live in the (famous building) and build a (gerundio) pool at her feet. \n4. School would be open for (number) days a year. \n5. I would give my friends the best jobs. I would nominate (a friend) to be the secretary of (the) (noun), and (another friend) would be vice (occupation)!!!")
