29efd2ea21b1965b032b95d22ede68a463b0c24d
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-09-05 09:53:20 -0600

    not done idiot proof smh

---
+++ projects/idiot_proof.py
@@ -0,0 +1,10 @@
+# GNB - 1st - 🔨 Idiot Proof
+
+nombre_primero = input("Hello, user! What's your first name?: ").strip().title()
+nombre_segundo = input("What about your last name?: ").strip().title
+
+numero_telefono = input("Now may I have your phone number 😏: ").strip()
+
+grado = input(float("Now what's your current GPA in school?: ".strip()))
+
+print("Name: "+ nombre_primero, nombre_segundo +"\n"+ "Phone: "+ numero_telefono +"\n"+ "GPA: "+ grado + "\n")
