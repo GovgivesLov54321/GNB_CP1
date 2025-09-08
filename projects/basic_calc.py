6d297237fa93bdc077b6d87181ea453fc7900577
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-09-08 09:53:30 -0600

    basic calc and f strings notes frfr

---
+++ projects/basic_calc.py
@@ -0,0 +1,10 @@
+# GNB - 1st - 🔨 Basic Calculator
+
+numero_1 = input(float("Give me one number:"))
+numero_2 = input(float("Give me another number:"))
+
+add = numero_1 + numero_2
+sub = numero_1 - numero_2
+multi = numero_1 * numero_2
+div = numero_1/numero_2
+int_div = numero_1%numero_2
