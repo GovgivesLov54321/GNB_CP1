63f3f9c5d6fc662c8c4e014ecbdddc2857309ce9
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-09-09 09:31:37 -0600

    made changes to basic calc

--- projects/basic_calc.py
+++ projects/basic_calc.py
@@ -1,10 +1,13 @@
 # GNB - 1st - 🔨 Basic Calculator
 
-numero_1 = input(float("Give me one number:"))
-numero_2 = input(float("Give me another number:"))
+numero_1 = input("Give me one number:")
+numero_2 = input("Give me another number:")
 
-add = numero_1 + numero_2
+add = float(numero_1 + numero_2)
 sub = numero_1 - numero_2
 multi = numero_1 * numero_2
 div = numero_1/numero_2
-int_div = numero_1%numero_2
+floor_div= numero_1//numero_2
+mod = numero_1%numero_2
+
+print(mod, floor_div, div, multi, sub, add)
