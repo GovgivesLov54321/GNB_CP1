9ec0c6c55bdc2099477b80846591e0bbae16fc85
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-09-09 09:49:34 -0600

    basic calculator lite frfr :P

--- projects/basic_calc.py
+++ projects/basic_calc.py
@@ -1,13 +1,20 @@
 # GNB - 1st - 🔨 Basic Calculator
 
-numero_1 = input("Give me one number:")
-numero_2 = input("Give me another number:")
+numero_1 = float(input("Give me one number:"))
+numero_2 = float(input("Give me another number:"))
 
-add = float(numero_1 + numero_2)
-sub = numero_1 - numero_2
-multi = numero_1 * numero_2
-div = numero_1/numero_2
-floor_div= numero_1//numero_2
-mod = numero_1%numero_2
+add = round(numero_1 + numero_2, 2)
+sub = round(numero_1 - numero_2, 2)
+multi = round(numero_1 * numero_2, 2)
+div = round(numero_1 / numero_2, 2)
+floor_div= round(numero_1 // numero_2, 2)
+mod = round(numero_1 % numero_2, 2)
+expo = round(numero_1 ** numero_2, 2)
 
-print(mod, floor_div, div, multi, sub, add)
+print(f"{numero_1} + {numero_2} = {add}")
+print(f"{numero_1} - {numero_2} = {sub}")
+print(f"{numero_1} x {numero_2} = {multi}")
+print(f"{numero_1} / {numero_2} = {div}")
+print(f"{numero_1} // {numero_2} = {floor_div}")
+print(f"{numero_1} % {numero_2} = {mod}")
+print(f"{numero_1} ** {numero_2} = {expo}")
