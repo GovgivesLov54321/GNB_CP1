81aadcda44357b44d4b35e950fadba2344db0793
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-09-10 09:48:58 -0600

    random numbers notes smh

---
+++ notes/random_numbers.py
@@ -0,0 +1,25 @@
+# GNB - 1st - ☑️Random Numbers Notes
+
+import random
+
+# Generate stat options
+stat_one = random.randint(1, 10) + random.randint(1,10)
+stat_two = random.randint(1, 10) + random.randint(1,10)
+stat_three = random.randint(1, 10) + random.randint(1,10)
+stat_four = random.randint(1, 10) + random.randint(1,10)
+stat_five = random.randint(1, 10) + random.randint(1,10)
+stat_six = random.randint(1, 10) + random.randint(1,10)
+stat_seven = random.randint(1, 10) + random.randint(1,10)
+
+# Telling user the stat choices
+print(f"Your stat options are: {stat_one}, {stat_two}, {stat_three}, {stat_four}, {stat_five}, {stat_six}, {stat_seven}.")
+
+# Stat finalization
+strength = int(input("Which stat do you want for your strength: \n"))
+aura = input("Which stat do you want for your aura: \n")
+strength = input("Which stat do you want for your strength: \n")
+strength = input("Which stat do you want for your strength: \n")
+strength = input("Which stat do you want for your strength: \n")
+
+print(random.random())
+print(f"You've rolled the dice, and it landed on the number {random.randint(1,6)}...")
