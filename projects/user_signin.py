4328cfc2a74d76ced7c50108c1fa6b4908ea3589
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-09-22 09:53:09 -0600

    I think I'm done with user sign in

---
+++ projects/user_signin.py
@@ -0,0 +1,13 @@
+# GNB - 1st - 🔨 User Sign In
+
+true_user = ("vienna.larose@ucas-edu.net")
+true_password = ("he_a_g00fy-goob3R")
+username = input("Enter username: \n")
+password = input("Enter password: \n")
+
+if username != true_user and password != true_password:
+    print("Username and password are invalid")
+elif username != true_user or password != true_password:
+    print("Username or password is invalid")
+else:
+   print("Welcome to the program, User.")
