f80851669c8afc56875c859e4bfd001d067ad954
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-09-25 09:53:13 -0600

    debugging notes

---
+++ notes/debugging.py
@@ -0,0 +1,12 @@
+# GNB - 1st - Debugging Notes
+
+#Syntax Error - grammar error
+    #print("Vienna")
+# => Read error message, go to that line of code, fix the syntax
+
+# Logic Errors - gave the wrong steps
+numone = "2"
+numtwo = "5"
+
+print(numone + numtwo)
+# Have well thought out plan, step by step go through our logic
