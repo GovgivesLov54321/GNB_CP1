be8c79e6d6e4e57221f1d14fc474abe94b0b4c22
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-09-30 02:37:13 +0000

    finally finished this dingdang fix game assignment

--- projects/fix_game.py
+++ projects/fix_game.py
@@ -6,14 +6,13 @@ def start_game():
     print("I'm thinking of a number between 1 and 100.")
     number_to_guess = random.randint(1, 100)
     max_attempts = 10
-    attempts = 0
+    attempts = 0 #Also mention attempts variable before saying that you'll add on to it
     game_over = False
     while not game_over:
-        guess = int(input("Enter your guess: \n")) #Didn't make the input an integer - syntax - it wasn't able to accept answers that weren't strings, but the answers had to be integers
-        if attempts >= max_attempts:
-            print(f"Sorry, you've used all {max_attempts} attempts. The number was {number_to_guess}.")
-            game_over = True
-        else: pass
+        attempts += 1 
+    #Never made it so that the amount of attempts used could go up - would've allowed user to guess forever without hitting the max attempts - logic
+        guess = int(input("Enter your guess: \n")) 
+    #Didn't make the input an integer - runtime - it wasn't able to accept answers that weren't strings, (it would crash) but the answers had to be integers
         if guess == number_to_guess:
             print("Congratulations! You've guessed the number!")
             game_over = True
@@ -21,6 +20,11 @@ def start_game():
             print("Too high! Try again.")
         elif guess < number_to_guess:
             print("Too low! Try again.")  
-        continue
+        if attempts >= max_attempts: 
+        #Shouldn't be checking if it used an attempt before checking if the guess is (in)correct (if it were last attempt used, the code would say that the user had run out of guesses before saying it was correct, and it'd be all messed up) - logic
+            print(f"Sorry, you've used all {max_attempts} attempts. The number was {number_to_guess}.")
+            game_over = True
+        else: pass
+    #Remove the "continue" thing - it does nothing for the code, and when the game is eventually over, it causes the code to skip over the game over... sentence - logic
     print("Game Over. Thanks for playing!")
 start_game()
