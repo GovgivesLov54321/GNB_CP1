6bcf310e132ebdbad8ba82ab6c4b2894f1f351f7
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-10-20 09:52:56 -0600

    functions notes fr

---
+++ notes/functions.py
@@ -0,0 +1,72 @@
+# GNB - 1st - ☑️Functions Notes
+#Set variables
+
+#define functions
+def add(x, y):
+    #print(f"{x}+{y} = {x+y}")
+    return x + y
+
+def initials(name):
+    names = name.split(" ")
+    initials = ""
+    for name in names:
+        initials += name [0]
+
+total = add(5,5)
+print(total)
+print(f" 10 + 72 = {add(10,72)}")
+x = 0
+while x < add(3,9):
+    print("duck")
+    x += 1
+
+print("Goose")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
