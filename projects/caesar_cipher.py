118d033b8bd42973bd3bd20c7459ea03d12c0841
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-10-22 09:52:41 -0600

    goofy ah assignment

---
+++ projects/caesar_cipher.py
@@ -0,0 +1,66 @@
+# GNB - 1st - 🔨 Caesar Cipher Encoder and Decoder
+
+#Ask for encode or decode
+choice = input("Would you like to encode or decode a message (encode or decode)?:\n").strip().capitalize()
+
+#define the dang functions
+
+
+if choice == "Encode":
+    def encode():
+        67   
+elif choice == "Decode":
+    def decode():
+        67
+else:
+    print("Not an option, buddy.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
