4f67a6d14821fa6b46a22c5a0d56f79c2045d9b2
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-11-05 09:52:11 -0700

    hate this so much cuddy

--- projects/maze_generator.py
+++ projects/maze_generator.py
@@ -8,6 +8,14 @@ maze_drawer1.shape("circle")
 maze_drawer2 = t.Turtle()
 maze_drawer2.shape("circle")
 
+grid_rows = []
+rows = []
+grid_col = []
+col = []
+
+for row in rows:
+    []
+
 
 def set_up():
     maze_drawer1.teleport(-400, 400)
@@ -18,8 +26,8 @@ def set_up():
     maze_drawer2.forward(400)
     maze_drawer1.left(90)   
     maze_drawer2.left(90)
-    maze_drawer1.forward(365)   
-    maze_drawer2.forward(365)
+    maze_drawer1.forward(370)   
+    maze_drawer2.forward(370)
     maze_drawer1.hideturtle()   
     maze_drawer2.hideturtle()
  
@@ -30,9 +38,9 @@ def walls():
 
 walls()
 
-def maze_layout():
+def is_solvable():
     help
 
-maze_layout()
+is_solvable()
 
 t.done()
