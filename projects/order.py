d9cf32ec2b867801c3b41fcc160b0b746b9820a4
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-11-13 09:52:27 -0700

    cool ig

--- projects/order.py
+++ projects/order.py
@@ -31,91 +31,37 @@ menu = {
          "Gameday Nacho Plate" : 7.00
     },
     "Sides" : {
-         "Chips - 3.75",
-         "(Spicy) Breakfast Potatoes - 4.75",
-         "Salsa - 2.00",
-         "Guacamole - 3.00",
-         "Sour Cream - 1.75",
-         "Flour/Corn Tortilla - 2.00",
-         "Beans - 3.00",
-         "Refried Beans - 3.75",
-         "Plantain Chips - 4.00"
+         "Chips" : 3.75,
+         "(Spicy) Breakfast Potatoes" : 4.75,
+         "Salsa Roja" : 2.00,
+         "Salsa Verde" : 2.25,
+         "Guacamole" : 3.00,
+         "Sour Cream" : 1.75,
+         "Flour/Corn Tortilla" : 2.00,
+         "Black Beans" : 3.00,
+         "Pinto Beans" : 3.00,
+         "Refried Black Beans" : 3.75,
+         "Refried Pinto Beans" : 3.75,
+         "Plantain Chips" : 4.00
     },
     "Desserts" : {
-         "Tres Leches Cake Slice - 6.00",
-         "Ice Cream - 5.00",
-         "Chocolate Chip Cookies - 4.50",
-         "Flan Slice - 5.00",
-         "Banana Pudding - 6.55",
-         "Churros - 6.40",
-         "Cinnamon/Strawberry/Cheese Cheesecake Slice - 5.65"
+         "Tres Leches Cake Slice" : 6.00,
+         "Ice Cream" : 5.00,
+         "Chocolate Chip Cookies" : 4.50,
+         "Flan Slice" : 5.00,
+         "Banana Pudding" : 6.55,
+         "Churros" : 6.40,
+         "Cinnamon/Strawberry/Cheese Cheesecake Slice" : 5.65
     }
 }
 print("Hello, User! Welcome to a place to get food!")
 
-for Drink in menu
-    print(f"{menu['Drinks']}")
-drink_choice = input(f"What drink would you like to have \n")
+drink_choice = input(f"What drink would you like to have: \n")
+for menu['Drink'] in menu["Drinks"]:
+     print(menu["Drink"])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for menu['Drink'] in menu['Drinks']:
+            print(f"{menu['Drink']} costs {menu['Drinks'[value]]}")
 
 
 
