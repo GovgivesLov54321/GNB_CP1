735f637ab0c19950042c356841bee9486652f435
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-11-18 09:52:29 -0700

    good job ig

--- projects/flexible_calculator.py
+++ projects/flexible_calculator.py
@@ -1,5 +1,28 @@
 # GNB - 1st - 🔨 Flexible Calculator
 
-numbers = input("Give any amount of numbers"); {
 
-}
+print("Welcome to the Flexible Calculator.\nThe avaiable operations that can be calculated are Sum, Average, Maximum, Minimum, or Product.")
+while True:
+    operation_choice = input("Which operation would you like to be performed?:\n").strip().capitalize()
+    if operation_choice == "Sum" or operation_choice == "Average" or operation_choice == "Maximum" or operation_choice == "Minimum" or operation_choice == "Product":
+        print("Great choice!")
+    else:
+        print("Not an option, buddy.")
+        continue
+   
+
+    num_choices = input("Enter a number (enter done, when done):\n")
+
+    if num_choices == "done":
+        break    
+    elif num_choices == 0:
+        print("Cool number")
+    else:
+        print("Not an option, buddy. Try again.")
+
+    def calculations(sum, average, maximum, minimum, product):
+        sum = (num_choices + num_choices)
+    calculations()
+
+    if num_choices == "done":
+        print(calculations(f"The sum of the numbers you gave is {sum}"))
