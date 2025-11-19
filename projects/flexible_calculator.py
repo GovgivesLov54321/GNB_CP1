c5118990e579ec317b50b1cabe71e4f18c498592
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-11-19 09:51:56 -0700

    never doing another comp class istb

--- projects/flexible_calculator.py
+++ projects/flexible_calculator.py
@@ -4,25 +4,52 @@
 print("Welcome to the Flexible Calculator.\nThe avaiable operations that can be calculated are Sum, Average, Maximum, Minimum, or Product.")
 while True:
     operation_choice = input("Which operation would you like to be performed?:\n").strip().capitalize()
-    if operation_choice == "Sum" or operation_choice == "Average" or operation_choice == "Maximum" or operation_choice == "Minimum" or operation_choice == "Product":
+    
+    if operation_choice == "Sum":
+        print("Great choice!")    
+        def sum(*args):
+            help    
+        sum()
+    elif operation_choice == "Average":
         print("Great choice!")
+        def average(*args):
+            sum 
+        average()
+    elif operation_choice == "Maximum":
+        print("Great choice!")
+        def maximum(*args):
+            sum 
+        maximum()
+    elif operation_choice == "Minimum":
+        print("Great choice!")
+        def minimum(*args):
+            sum 
+        minimum()
+    elif operation_choice == "Product":
+        print("Great choice!")
+        def product(*args):
+            sum 
+        product()
     else:
-        print("Not an option, buddy.")
+        print("Not an option, buddy. Try again.")
         continue
-   
 
+    num_choices_list = []
     num_choices = input("Enter a number (enter done, when done):\n")
 
+    for num_choice in num_choices:
+        num_choices_list.append(num_choices)
+    print(num_choices_list)    
+    
+
     if num_choices == "done":
-        break    
-    elif num_choices == 0:
+        print(f"Now lets calculate the {operation_choice} of those numbers!")    
+   # elif num_choices is :
         print("Cool number")
     else:
         print("Not an option, buddy. Try again.")
 
-    def calculations(sum, average, maximum, minimum, product):
-        sum = (num_choices + num_choices)
-    calculations()
 
-    if num_choices == "done":
-        print(calculations(f"The sum of the numbers you gave is {sum}"))
+
+
+        
